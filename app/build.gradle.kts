3063840305ccae45590b6becdcf1783650301d8a
Alan Huang <lahuangao@gmail.com>
2026-07-19 06:00:49 -0400

    chore: prepare 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.voyagerfiles"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.4.0"
+        versionCode = 13
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
